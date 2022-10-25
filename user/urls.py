86c71527b66036919357efecdd2961ba2ccc4069
josh <snoopyjsc@gmail.com>
2022-10-25 15:14:18 +0800

    完成登入功能

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from .import views
-
+from . import views
+# http://127.0.0.1:8000/user/register
 urlpatterns = [
-    path('register/', views.user_register),
-
+    path('register/', views.user_register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('', views.profile, name='profile')
 
 ]
