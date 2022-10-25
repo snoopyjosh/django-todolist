c8073c5421b820fe4101d8e7086cb3f3661721b6
josh <snoopyjsc@gmail.com>
2022-10-25 15:14:18 +0800

    完成登入功能

--- user/views.py
+++ user/views.py
@@ -1,5 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+
+def user_logout(requset):
+    logout(requset)
+    return redirect('profile')
+
+
+def profile(request):
+    return render(request, './user/profile.html')
+
+
+def user_login(request):
+    message, username = '', ''
+    # 是否是POST表單
+    if request.method == 'POST':
+        if request.POST.get('login'):
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            print(username, password)
+            # 1.檢查帳號密碼是否為空
+            if username == '' or password == '':
+                message = '帳號跟密碼不能為空!'
+            else:
+                # 2.是否有該使用者
+                # 3.匹配密碼進行登入
+                user = authenticate(
+                    request, username=username, password=password)
+                if user is None:
+                    if User.objects.filter(username=username):
+                        message = '密碼有誤!'
+                    else:
+                        message = '帳號有誤!'
+                else:
+                    # 進行登入
+                    login(request, user)
+                    message = '登入中...'
+                    return redirect('profile')
+
+        elif request.POST.get('register'):
+            return redirect('register')
+
+    return render(request, './user/login.html', {'message': message, 'username': username})
+
 
 # Create your views here.
 def user_register(request):
-    return render(request,'./')
+    # 產生使用者表單
+    form = UserCreationForm()
+    message = ''
+
+    # 檢查是get or post
+    if request.method == 'GET':
+        print('GET')
+    elif request.method == 'POST':
+        print('POST')
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        # 註冊功能
+        # 密碼不能少於8各字元
+        # 兩次密碼是否相同
+        # 使用者名稱不能重複
+        # 進行註冊
+        if len(password1) < 8:
+            message = '密碼少於8各字元'
+        elif password1 != password2:
+            message = '兩次密碼不同'
+        else:
+            if User.objects.filter(username=username).exists():
+                message = '帳號重複'
+            else:
+                User.objects.create_user(
+                    username=username, password=password1).save()
+                message = '註冊成功!'
+
+    return render(request, './user/register.html', {'form': form, 'message': message})
